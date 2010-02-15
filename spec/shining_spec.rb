09950761fa30f3b0fb70e453e13bde2e1cd8753c
Julio Cesar Ody <julio.ody@nexuspoint.com.au>
2010-02-16 09:11:47 +1100

    passing!

--- spec/shining_spec.rb
+++ spec/shining_spec.rb
@@ -1,16 +1,16 @@
 require File.join(File.dirname(__FILE__), *%w(.. .bundle environment))
 Bundler.require(:default, :test)
 
-__ROOT__ = File.join(File.dirname(__FILE__), '..')
+__ROOT__ = File.expand_path File.join(File.dirname(__FILE__), '..')
 
 require 'spec'
 
 describe 'Shining' do  
   before do
-    @page = Harmony::Page.fetch("file:///#{__ROOT__}/spec/sample/home.html")
+    @page = Harmony::Page.fetch("file:////#{__ROOT__}/spec/sample/home.html")
   end
   
-  it "loads the first page when opening" do
-    
+  it "has a stage" do
+    @page.execute_js("$('#stage').is(':visible')").should == true
   end  
 end
