60da61a8e215c5ca7001ca1e4c929a94a28a5690
Julio Cesar Ody <julio.ody@nexuspoint.com.au>
2010-02-16 11:20:49 +1100

    RORO preso plan

--- shining.js
+++ shining.js
@@ -5,10 +5,6 @@
     String.prototype.markup = function() { return this + '.html' };
     String.prototype.script = function() { return this + '.js' };
     
-    function page(number) {
-      return 'pages/' + $.shining.pages[number];
-    };
-    
     $.extend($.shining, {
       firstSlide:     function() { getSlide(0) },
       nextSlide:      function() { getSlide($.shining.current++) },
@@ -16,31 +12,34 @@
       page: page
     });
     
-    function init() {
-      fetchPages(function() { getSlide(0) });
-    };
+    function page(number) { return 'pages/' + $.shining.pages[number] }
+    function init()       { fetchPages(function() { getSlide(0) }) }
     
     function fetchPages(callback) {
       $.getJSON('pages.json', function(pages) {
         $.shining.pages = pages;
         callback.call();
       });
-    };
+    }
     
     function getSlide(number) {
-      $('#stage').load(page(number).markup());
-      $.get(page(number).script(), function(script) { with($.shining.context) { eval(script) } });
+      $('#stage').load(
+        page(number).markup(),
+        function() {
+          $.get(page(number).script(), function(script) { with($.shining.context) { eval(script) } });
+        }
+      );
     }
             
     init();
-  };
+  }
   
   // gives page scripts a context for execution
   $.shining.context = $.noop;
   with ($.shining.context) {
-    this.at =             function(seconds, method) { setTimeout(method, parseInt(seconds) * 1000) };
-    this.nextSlide =      function() { $.shining.nextSlide(); };
-    this.previousSlide =  function() { $.shining.previousSlide(); };
+    this.at            = function(seconds, method) { setTimeout(method, parseInt(seconds) * 1000) };
+    this.nextSlide     = function() { $.shining.nextSlide() };
+    this.previousSlide = function() { $.shining.previousSlide() };
   }
   
   // boots!
