b40283e62900553513bb2bf7a38252b8d6bd72ca
Julio Cesar Ody <julio.ody@nexuspoint.com.au>
2010-02-17 13:12:42 +1100

    saner dirs, specs, names

--- spec/shining_spec.rb
+++ spec/shining_spec.rb
@@ -7,10 +7,33 @@ require 'spec'
 
 describe 'Shining' do  
   before do
+    Dir.chdir File.join(__ROOT__, *%w(spec sample))
     @page = Harmony::Page.fetch("file:////#{__ROOT__}/spec/sample/home.html")
   end
   
   it "has a stage" do
     @page.execute_js("$('#stage').is(':visible')").should == true
-  end  
+  end
+  
+  describe ".slides" do
+    it ".current should return the first slide by default" do
+      @page.execute_js("$.shining.slides.current").should == 'first'
+    end
+    
+    it "sets a slide via .current= returning the slide name" do
+      @page.execute_js("$.shining.slides.current = 'second'").should == 'second'
+    end    
+    
+    it ".current returns undefined if set to an unexisting slide" do
+      @page.execute_js("$.shining.slides.current = 'omg'; $.shining.slides.current").should == nil
+    end
+    
+    it "returns the next slide in the list on .next" do
+      @page.execute_js("$.shining.slides.current = 'second'; $.shining.slides.next").should == 'third'
+    end
+    
+    it "returns the previous slide in the list on .previous" do
+      @page.execute_js("$.shining.slides.current = 'second'; $.shining.slides.previous").should == 'first'
+    end
+  end
 end
