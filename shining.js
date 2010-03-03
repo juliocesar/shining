a28943afe4859f2b7e4aa0c7892864368f6d59f7
Julio Cesar Ody <julio.ody@nexuspoint.com.au>
2010-03-03 15:20:38 +1100

    many... different... kinds... of... effects!

--- shining.js
+++ shining.js
@@ -158,14 +158,7 @@
 (function(b){var a=function(c){return parseInt(c,10)||0};b.each(["min","max"],function(d,c){b.fn[c+"Size"]=function(g){var f,e;if(g){if(g.width!==undefined){this.css(c+"-width",g.width)}if(g.height!==undefined){this.css(c+"-height",g.height)}return this}else{f=this.css(c+"-width");e=this.css(c+"-height");return{width:(c==="max"&&(f===undefined||f==="none"||a(f)===-1)&&Number.MAX_VALUE)||a(f),height:(c==="max"&&(e===undefined||e==="none"||a(e)===-1)&&Number.MAX_VALUE)||a(e)}}}});b.fn.isVisible=function(){return this.is(":visible")};b.each(["border","margin","padding"],function(d,c){b.fn[c]=function(e){if(e){if(e.top!==undefined){this.css(c+"-top"+(c==="border"?"-width":""),e.top)}if(e.bottom!==undefined){this.css(c+"-bottom"+(c==="border"?"-width":""),e.bottom)}if(e.left!==undefined){this.css(c+"-left"+(c==="border"?"-width":""),e.left)}if(e.right!==undefined){this.css(c+"-right"+(c==="border"?"-width":""),e.right)}return this}else{return{top:a(this.css(c+"-top"+(c==="border"?"-width":""))),bottom:a(this.css(c+"-bottom"+(c==="border"?"-width":""))),left:a(this.css(c+"-left"+(c==="border"?"-width":""))),right:a(this.css(c+"-right"+(c==="border"?"-width":"")))}}}})})(jQuery);
 
 // http://github.com/juliocesar/jquery-ondistance
-(function($) {$.fn.ondistance=function(specified,close,far){var elt=this.get(0),last,offset=$(elt).offset(),center={x:offset.left+($(elt).width()/2),y:offset.top+($(elt).height()/2)};$(document).mousemove(function(e){var current=new Date().getTime();if(current-last<500){return}last=current;var distance=parseInt(Math.sqrt(Math.pow(e.pageX-center.x,2)+Math.pow(e.pageY-center.y,2)));if(specified>=distance){if($(elt).data("mouseclose")==true){return false}$(elt).data("mouseclose",true);close(elt)}else{if($(elt).data("mouseclose")==false){return false}$(elt).data("mouseclose",false);far(elt)}})};})(jQuery);
+(function($){$.fn.ondistance=function(specified,close,far){var elt=this.get(0),last=new Date().getTime();$(document).mousemove(function(e){var current=new Date().getTime();if(current-last<500)return;var offset=$(elt).offset(),center={x:offset.left+($(elt).width()/2),y:offset.top+($(elt).height()/2)},last=current,distance=parseInt(Math.sqrt(Math.pow(e.pageX-center.x,2)+Math.pow(e.pageY-center.y,2)));if(specified>=distance){if($(elt).data('mouseclose')==true)return false;$(elt).data('mouseclose',true);close(elt);}else{if($(elt).data('mouseclose')==false)return false;$(elt).data('mouseclose',false);far(elt);}})}})(jQuery);
 
-// 
-(function($) {
-  $.fn.centralize = function() {
-    var self = this.get(0);
-      windowHeight = document.documentElement.clientHeight, 
-      elementHeight = (self.offsetHeight + $(self).padding().top + $(self).padding().bottom);
-    $(self).css('position', 'relative').css('top', (windowHeight / 2) - (elementHeight / 2) + 'px');      
-  };
-})(jQuery);
+// centralizes an element vertically
+(function($){$.fn.centralize=function(){var self=this.get(0);windowHeight=document.documentElement.clientHeight,elementHeight=(self.offsetHeight+$(self).padding().top+$(self).padding().bottom);$(self).css('position','relative').css('top',(windowHeight/2)-(elementHeight/2)+'px');};})(jQuery);
--- slides/effects.js
+++ slides/effects.js
@@ -1,1 +1,19 @@
-1,    $('h2:first').addClass('speeds-in-right')
+1,    $('h2:first').addClass('appears-dramatically')
+3.6,
+      $('h2:first').hide()
+      $('h2:eq(1)').removeClass('hidden');
+3.7,  
+      $('h2:eq(1)').addClass('speeds-in-left')
+5.7,
+      $('h2:eq(1)').hide()
+      $('h2:eq(2)').removeClass('hidden').addClass('speeds-in-right')
+7.8,
+      $('h2:eq(2)').hide()
+      $('h2:eq(3)').removeClass('hidden').addClass('fades-in')
+8.5,  $('h2:eq(3)').addClass('y-spin')
+10.5, 
+      $('h2:eq(3)').hide()
+      $('h2:eq(4)').removeClass('hidden')
+10.7,
+      $('h2:eq(4)').removeClass('transparent').addClass('fades-in')
+      $('h2:eq(4)').addClass('x-spin')
