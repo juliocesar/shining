cc9ba1669984ae5d1e01d1fbd5639aca82f102c4
Julio Cesar Ody <julioody@gmail.com>
2010-03-14 17:02:41 +1100

    de-dumbed the gem requirements

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,9 @@ require File.join(File.dirname(__FILE__), *%w(.. lib shining))
 
 gem 'stackdeck',  '0.2.0';          require 'stackdeck'
 gem 'johnson',    '2.0.0.pre2';     require 'johnson'
-gem 'rspec',      '1.3.0';          require 'spec'
-gem 'rake',       '0.8.7';          require 'rake'
+gem 'rspec',      '>= 1.3.0';       require 'spec'
+gem 'rake',       '>= 0.8.7';       require 'rake'
 gem 'envjs',      '0.1.4';          require 'envjs'
 gem 'juliocesar-harmony', '0.5.2';  require 'harmony'
-gem 'json_pure',  '1.1.9';          require 'json/pure'
+gem 'json_pure',  '>= 1.1.9';       require 'json/pure'
+gem 'haml',       '>= 2.2.17';      require 'haml'
